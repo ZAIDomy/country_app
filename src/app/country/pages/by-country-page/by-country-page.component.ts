55accce057ab2fd3b2e0becb7a2ad87000e761d2
Omar Diaz <odiaz.dev@gmail.com>
2025-05-09 11:08:38 -0600

    nuevas rutas y agregar componentes

---
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SearchInputComponent } from "../../components/search-input/search-input.component";
+import { CountryListComponent } from "../../components/country-list/country-list.component";
+
+@Component({
+  selector: 'by-country-page',
+  standalone: true,
+  imports: [SearchInputComponent, CountryListComponent],
+  templateUrl: './by-country-page.component.html',
+})
+export class ByCountryPageComponent { }
