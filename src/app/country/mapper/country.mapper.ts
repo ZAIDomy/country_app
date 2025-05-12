b0885db509963267130aecfee7e016004961b9b2
Omar Diaz <odiaz.dev@gmail.com>
2025-05-12 10:15:20 -0600

    country mapper

--- src/app/country/mapper/country.mapper.ts
+++ src/app/country/mapper/country.mapper.ts
@@ -1,8 +1,21 @@
+import { Country } from '../interfaces/country.interface';
 import { RESTCountry } from '../interfaces/rest-country.interface';
+
 export class CountryMapper{
 
-  static mapRestCountrytoCountry(restCountry: RESTCountry){
+  static mapRestCountrytoCountry(restCountry: RESTCountry):Country{
+    return {
+      capital: restCountry.capital.join(','),
+      cca2: restCountry.cca2,
+      flag: restCountry.flag,
+      flagSvg: restCountry.flags.svg,
+      name: restCountry.name.common,
+      population:restCountry.population,
+    };
+  }
 
+  static mapRestCountryArraytoCountryArray(restCountry: RESTCountry[]):Country[]{
+    return restCountry.map(this.mapRestCountrytoCountry);
   }
 
 
