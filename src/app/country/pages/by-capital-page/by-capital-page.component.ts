00b7f483a45ea5c06a45cc9819a7b29b6bfdb445
Omar Diaz <odiaz.dev@gmail.com>
2025-05-12 12:49:44 -0600

    reactividad con "resources"

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
     selector: 'app-by-capital-page',
@@ -12,8 +13,20 @@ import { Country } from '../../interfaces/country.interface';
 export class ByCapitalPageComponent {
 
   countryService=inject(CountryService);
+  query=signal('');
 
-  isLoading=signal(false);
+  countryResourse = resource({
+    request: ()=>({ query: this.query()}),
+    loader: async ({request})=>{
+      if(!request.query) return [];
+
+      return await firstValueFrom(
+        this.countryService.searchByCapital(request.query)
+      );
+    }
+  })
+
+/*   isLoading=signal(false);
   isError=signal<string|null>(null);
   countries=signal<Country[]>([]);
 
@@ -36,6 +49,6 @@ export class ByCapitalPageComponent {
         this.isError.set(err);
       }
     })
-  }
+  } */
 
  }
