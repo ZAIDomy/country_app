0a69b43b1de9b6b7ea8c1e78f784b4002e97bad2
Omar Diaz <odiaz.dev@gmail.com>
2025-05-15 14:57:41 -0600

    debounce prev 2

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -17,7 +17,7 @@ export class CountryService {
   searchByCapital(query: string):Observable<Country[]>{
 
     query=query.toLowerCase();
-
+    console.log(`emitiendo un valor ${query}`);
     return of([]);
 
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
